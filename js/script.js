aaf5f9dcb07febd0bc83a6aad1b2c834c5ee17eb
sjwestbrook <sawest4291@gmail.com>
2015-09-27 18:21:18 -0600

    responsive updates

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+  $('#slides').slidesjs({
+    height: 235,
+    navigation: false,
+    pagination: false,
+    effect: {
+      fade: {
+        speed: 400}},callback:
+        {
+          start: function(number)
+          {
+            $("#slider_content1,#slider_content2,#slider_content3").fadeOut(500);},
+            complete: function(number)
+            {
+              $("#slider_content" + number).delay(500).fadeIn(1000);}},
+              play:
+              {
+                active: false,
+                auto: true,
+                interval: 6000,
+                pauseOnHover: false}});
+  });
+
+  $("#alternative_menu").change(function() {
+    $selected = $("#alternative_menu option:selected");
+    window.location.href = "http://www.lingulo.com/" + $selected.val();
+  });
